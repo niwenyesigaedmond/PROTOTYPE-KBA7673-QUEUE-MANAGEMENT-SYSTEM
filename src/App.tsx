7a4d150eecbe95eb40a24cb8b72d438b69a5f4d2
git config --global user.email <you@example.com>
2025-11-27 19:24:50 +0300

    gg

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/PROTOTYPE-KBA7673-QUEUE-MANAGEMENT-SYSTEM">
           <Routes>
             <Route path="/" element={<Auth />} />
             <Route path="/dashboard" element={<Index />} />
